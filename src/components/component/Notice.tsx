0c13f8e7f5d046d17737018c50590d4f609b048a
JinSub <dev_fe_js@carenation.kr>
2025-04-07 16:09:28 +0900

    README update

--- src/components/component/Notice.tsx
+++ src/components/component/Notice.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 const DrawerInner = styled.div`
 	padding: 10px;
 	font-size: 15px;
+	margin-bottom: 5px;
 `
 
 const DrawerFooter = styled.div`
@@ -12,6 +13,16 @@ const DrawerFooter = styled.div`
 	bottom: 0;
 	font-size: 7px;
 	padding: 10px;
+`
+
+const DrawerInnerHeader = styled.div`
+	font-size: 16px;
+	font-weight: 600;
+	margin-bottom: 10px;
+`
+
+const BlackDiv = styled.div`
+	margin-bottom: 10px;
 `
 
 export const Notice = () => {
@@ -38,29 +49,32 @@ export const Notice = () => {
 				width={'30vw'}
 			>
 				<DrawerInner>
-					🍕 하단 입력창에 금액을 입력하면 잔액 및 남은 평균 일수 동안
-					얼마짜리를 먹을 수 있는지 알 수 있어요.
+					<DrawerInnerHeader>🍕 주 기능</DrawerInnerHeader>
+					하단 입력창에 금액을 입력하면 잔액 및 남은 평균 일수 동안 얼마짜리를
+					먹을 수 있는지 알 수 있어요.
 				</DrawerInner>
 				<DrawerInner>
-					🍟 달력에 클릭하여, 미리 지출할 금액을 입력할 수 있어요.
+					<DrawerInnerHeader>🍟 클릭!</DrawerInnerHeader>
+					달력에 클릭하여, 미리 지출할 금액을 입력할 수 있어요. <br />
+					미리 지출할 금액을 지우려면 0을 입력해요.
 				</DrawerInner>
 				<DrawerInner>
-					🌭 달력에 우클릭하여, 연차/야근식대/주말출근 등을 선택하여 추가 계산
-					할 수 있어요.
+					<DrawerInnerHeader>🌭 우클릭!</DrawerInnerHeader>
+					달력에 우클릭하여, 연차/야근식대/주말출근 등을 선택하여 추가 계산 할
+					수 있어요.
 				</DrawerInner>
 				<DrawerInner>
-					🍖 남은 근무일수는 점심먹기 전이라면 근무일수로, 점심을 먹은 뒤라면
+					<DrawerInnerHeader>🍖 근무일수 산정법</DrawerInnerHeader>
+					남은 근무일수는 점심먹기 전이라면 근무일수로, 점심을 먹은 뒤라면
 					근무일수에서 제외되요.
+					<DrawerInnerHeader> </DrawerInnerHeader>
+					오전 반차의 경우에도 점심식사를 하지 않으므로, 근무일수에서 제외되요.
 				</DrawerInner>
 				<DrawerInner>
-					🍙 오전 반차의 경우에도 점심식사를 하지 않으므로, 근무일수에서
-					제외되요.
-				</DrawerInner>
-				<DrawerInner>
-					🍕 달력에 근무, 주말 휴무는 기본값으로 표시되지 않아요.
-				</DrawerInner>
-				<DrawerInner>
-					🍟 너무 많은 데이터를 관리하지 않도록 해당 월만 확인되요.
+					<DrawerInnerHeader>🍙 달력 표시</DrawerInnerHeader>
+					달력에 근무, 주말 휴무는 기본값으로 표시되지 않아요.
+					<DrawerInnerHeader> </DrawerInnerHeader>
+					너무 많은 데이터를 관리하지 않도록 해당 월만 확인되요.
 				</DrawerInner>
 				<DrawerFooter>
 					* 해당 데이터는 참고용일뿐, 실데이터는 고위드 및 엑셀을 참고해주세요.
